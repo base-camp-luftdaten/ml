883dfb6a968e06df9aafca9f8c7b98934e495860
Lion Ralfs <lion.ralfs@hotmail.com>
2019-11-26 23:29:18 +0100

    started implementing apscheduler

--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 # -*- coding: utf-8 -*-
 """
 Created on Fri May 24 14:43:25 2019
@@ -34,6 +36,9 @@ import matplotlib.pyplot as plt
 import keras
 import tensorflow as tf
 
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+API_KEY = os.environ.get('API_KEY')
 
 # system specific values, as our batch size is fairly small cpu should be faster.
 # change these to fit your system
@@ -80,7 +85,7 @@ def getDataFromSensor(sensorID, timestamp):
         measurement = singleResult['measurement']
         weatherReport = singleResult['weatherReport']
 
-        if (weatherReport is None): 
+        if (weatherReport is None):
             # use the last available weatherReport instead
             weatherReport = lastWeatherReport
 
@@ -215,12 +220,11 @@ def loadModel(jsonpath, h5path):
     '''
     loading the model with a path
     '''
-    json_file = open(jsonpath, 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    loaded_model.load_weights(h5path)
-    return loaded_model
+    with open(jsonpath, 'r') as json_file:
+        loaded_model_json = json_file.read()
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights(h5path)
+        return loaded_model
 
 
 def furtherTraining(X_train, y_train, regressor):
@@ -278,7 +282,7 @@ def isContinuous(sensorID, timestamp):
     '''
 
     fullUrl = "http://basecamp-demos.informatik.uni-hamburg.de:8080/AirDataBackendService/api/measurements/bySensor?sensor=" + \
-        str(sensorID) + "&timestamp="+str(latestTimestamp)
+        str(sensorID) + "&timestamp="+str(timestamp)
 
     data = {}
 
@@ -343,37 +347,54 @@ def plot2(plot, plot2, name):
     plt.show()
 
 
-# giving the prediction to the backend
-key = os.environ.get('API_KEY')
-if (key == None):
+if (API_KEY == None):
     print("No key specified, printing predictions to console only.")
 
-sensorList = getSensorList()
-latestTimestamp = int(time.time())
 model = loadModel("regressor.json", "model.h5")
 
-for i, sensorId in enumerate(sensorList):
-    # check if process has been running for more than 50 minutes (= 3000 sec)
-    if (int(time.time()) - latestTimestamp > 3000):
-        print("Stopping execution because process took too long.")
-        break
+def createPredictions():
+    '''
+    Fetches all available sensor and attempts
+    to create a prediction for each sensor
+    after checking whether the sensor is continuous.
+    '''
+    sensorList = getSensorList()
+    latestTimestamp = int(time.time())
 
-    print(str(i) + " / " + str(len(sensorList)))
-    result = predictionGiver(sensorId, latestTimestamp, model)
-    if (type(result) != type(None)):
-        if (key == None):
-            print(result)
+    for i, sensorId in enumerate(sensorList):
+        # check if process has been running for more than 50 minutes (= 3000 sec)
+        if (int(time.time()) - latestTimestamp > 3000):
+            print("Stopping execution because process took too long.")
+            break
+
+        print(str(i) + " / " + str(len(sensorList)))
+        result = predictionGiver(sensorId, latestTimestamp, model)
+        if (type(result) != type(None)):
+            if (API_KEY == None):
+                print(result)
+            else:
+                try:
+                    requests.post('http://basecamp-demos.informatik.uni-hamburg.de:8080/AirDataBackendService/api/measurements/updatePredictions',
+                                  timeout=5,
+                                  json={"startTime": latestTimestamp,
+                                        "sensor": sensorId,
+                                        "values": result.tolist(),
+                                        "apiKey": API_KEY})
+                except (requests.exceptions.ReadTimeout, socket.timeout, requests.exceptions.ConnectTimeout) as e:
+                    print("Prediction upload took too long " +
+                          sensorId + " " + str(latestTimestamp))
+                    print(e)
         else:
-            try:
-                requests.post('http://basecamp-demos.informatik.uni-hamburg.de:8080/AirDataBackendService/api/measurements/updatePredictions',
-                              timeout=5,
-                              json={"startTime": latestTimestamp,
-                                    "sensor": sensorId,
-                                    "values": result.tolist(),
-                                    "apiKey": key})
-            except (requests.exceptions.ReadTimeout, socket.timeout, requests.exceptions.ConnectTimeout) as e:
-                print("Prediction upload took too long " +
-                      sensorId + " " + str(latestTimestamp))
-                print(e)
-    else:
-        print(sensorId + " is not continuous!")
+            print(sensorId + " is not continuous!")
+
+
+if __name__ == '__main__':
+    scheduler = BlockingScheduler()
+    scheduler.add_job(createPredictions, trigger='cron', minute='5',
+                      hour='*/1', max_instances=1)
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
